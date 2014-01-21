247d5cf124c1335efaee5b3862a77a5a5d7d850e
Magnus Achim Deininger <magnus@ef.gy>
2014-01-21 09:51:56 +0000

    started to get busy with documenting the OSX client

--- src/xstep/desktop/OSXAppDelegate.h
+++ src/xstep/desktop/OSXAppDelegate.h
@@ -1,4 +1,5 @@
-/**\file
+/**\ingroup frontend-cocoa
+ * \file
  * \brief OSX Application Delegate interface
  *
  * Contains the interface for the NSApplicationDelegate subclass of the OSX
@@ -40,7 +41,8 @@
 #define MAXDEPTH 7
 #endif
 
-/**\brief OSX application delegate
+/**\ingroup frontend-cocoa
+ * \brief OSX application delegate
  *
  * Contains the application delegate for the OSX version of Topologic, which
  * provides access to the C++ state object and a rather large number of
@@ -67,6 +69,7 @@
 @property (readonly) topologic::state<GLfloat,MAXDEPTH> *state;
 
 @property (assign) IBOutlet NSWindow *window;
+@property (retain,readwrite) IBOutlet NSPopUpButton *models;
 
 @property (retain,readwrite) OpenGLRenderer *openGL;
 
@@ -75,39 +78,242 @@
 @property (readonly) BOOL modelViewDrawerVisible;
 
 @property (readwrite) NSInteger drawerMode;
+
+/**\ingroup frontend-cocoa
+ * \defgroup osx-data-bindings Data bindings
+ * \brief Properties for XIB bindings
+ *
+ * Properties that make it easier to access parts of the programme state with
+ * Xcode's Interface Builder bindings. A lot of these could be simple functions,
+ * and usually they really only delegate to functions of the state object, but
+ * binding them to XIB components means we have to provide a simple property for
+ * them and we can't use functions that take an argument.
+ *
+ * All of these properties are exposed by the application delegate,
+ * \ref OSXAppDelegate.
+ *
+ * \{
+ */
+
+/**\defgroup osx-data-bindings-camera Camera position
+ * \brief Properties related to the current camera state
+ *
+ * These properties describe the current camera state and allow manipulation of
+ * the camera's position.
+ *
+ * \{
+ */
+
+/**\brief Active camera
+ *
+ * Used to query and set which camera should be manipulated with the UI controls
+ * right now. For example, setting this to 0 will allow you to control the 3D
+ * camera, setting this to 1 will allow you to control the 4D camera and so on.
+ */
 @property (readwrite) NSInteger activeCamera;
+
+/**\brief Coordinate system for active camera
+ *
+ * Used to query and set the currently active camera coordinate system. Possible
+ * values are:
+ *
+ *  - 0 for cartesian coordinates
+ *  - 1 for polar coordinates
+ */
 @property (readwrite) NSInteger activeCameraType;
 
-@property (retain,readwrite) NSColor *colourBackground;
-@property (retain,readwrite) NSColor *colourWire;
-@property (retain,readwrite) NSColor *colourSurface;
-
+/**\brief Active camera coordinate 0
+ *
+ * Used to query and set the currently active camera's 0th coordinate. This
+ * really should be a function that just takes the coordinate as input, but then
+ * Data bindings in the Interface Builder won't work.
+ */
 @property (readwrite) double cameraActiveCoordinate0;
+
+/**\brief Active camera coordinate 1
+ *
+ * Used to query and set the currently active camera's 1st coordinate. This
+ * really should be a function that just takes the coordinate as input, but then
+ * Data bindings in the Interface Builder won't work.
+ */
 @property (readwrite) double cameraActiveCoordinate1;
+
+/**\brief Active camera coordinate 2
+ *
+ * Used to query and set the currently active camera's 2nd coordinate. This
+ * really should be a function that just takes the coordinate as input, but then
+ * Data bindings in the Interface Builder won't work.
+ */
 @property (readwrite) double cameraActiveCoordinate2;
+
+/**\brief Active camera coordinate 3
+ *
+ * Used to query and set the currently active camera's 3rd coordinate. This
+ * really should be a function that just takes the coordinate as input, but then
+ * Data bindings in the Interface Builder won't work.
+ */
 @property (readwrite) double cameraActiveCoordinate3;
+
+/**\brief Active camera coordinate 4
+ *
+ * Used to query and set the currently active camera's 4th coordinate. This
+ * really should be a function that just takes the coordinate as input, but then
+ * Data bindings in the Interface Builder won't work.
+ */
 @property (readwrite) double cameraActiveCoordinate4;
+
+/**\brief Active camera coordinate 5
+ *
+ * Used to query and set the currently active camera's 5th coordinate. This
+ * really should be a function that just takes the coordinate as input, but then
+ * Data bindings in the Interface Builder won't work.
+ */
 @property (readwrite) double cameraActiveCoordinate5;
+
+/**\brief Active camera coordinate 6
+ *
+ * Used to query and set the currently active camera's 5th coordinate. This
+ * really should be a function that just takes the coordinate as input, but then
+ * Data bindings in the Interface Builder won't work.
+ */
 @property (readwrite) double cameraActiveCoordinate6;
 
+/**\brief Camera coordinate input 0 enabled?
+ *
+ * 'YES' when the 0th camera coordinate should currently be editable. This
+ * should probably be a single function instead of seven, but then we couldn't
+ * access the properties using Interface Builder.
+ */
 @property (readonly) BOOL cameraActiveCoordinate0Enabled;
+
+/**\brief Camera coordinate input 1 enabled?
+ *
+ * 'YES' when the 1st camera coordinate should currently be editable. This
+ * should probably be a single function instead of seven, but then we couldn't
+ * access the properties using Interface Builder.
+ */
 @property (readonly) BOOL cameraActiveCoordinate1Enabled;
+
+/**\brief Camera coordinate input 2 enabled?
+ *
+ * 'YES' when the 2nd camera coordinate should currently be editable. This
+ * should probably be a single function instead of seven, but then we couldn't
+ * access the properties using Interface Builder.
+ */
 @property (readonly) BOOL cameraActiveCoordinate2Enabled;
+
+/**\brief Camera coordinate input 3 enabled?
+ *
+ * 'YES' when the 3rd camera coordinate should currently be editable. This
+ * should probably be a single function instead of seven, but then we couldn't
+ * access the properties using Interface Builder.
+ */
 @property (readonly) BOOL cameraActiveCoordinate3Enabled;
+
+/**\brief Camera coordinate input 4 enabled?
+ *
+ * 'YES' when the 4th camera coordinate should currently be editable. This
+ * should probably be a single function instead of seven, but then we couldn't
+ * access the properties using Interface Builder.
+ */
 @property (readonly) BOOL cameraActiveCoordinate4Enabled;
+
+/**\brief Camera coordinate input 5 enabled?
+ *
+ * 'YES' when the 5th camera coordinate should currently be editable. This
+ * should probably be a single function instead of seven, but then we couldn't
+ * access the properties using Interface Builder.
+ */
 @property (readonly) BOOL cameraActiveCoordinate5Enabled;
+
+/**\brief Camera coordinate input 6 enabled?
+ *
+ * 'YES' when the 6th camera coordinate should currently be editable. This
+ * should probably be a single function instead of seven, but then we couldn't
+ * access the properties using Interface Builder.
+ */
 @property (readonly) BOOL cameraActiveCoordinate6Enabled;
 
+/**\brief UI label text for the 0th coordinate
+ *
+ * The label for the 0th coordinate that should be displayed next to its slider
+ * in the interface. It'd be nice if this could be a single function, but then
+ * Interface Builder bindings would no longer work.
+ */
 @property (retain,readonly) NSString *cameraActiveCoordinate0Label;
+
+/**\brief UI label text for the 1st coordinate
+ *
+ * The label for the 1st coordinate that should be displayed next to its slider
+ * in the interface. It'd be nice if this could be a single function, but then
+ * Interface Builder bindings would no longer work.
+ */
 @property (retain,readonly) NSString *cameraActiveCoordinate1Label;
+
+/**\brief UI label text for the 2nd coordinate
+ *
+ * The label for the 2nd coordinate that should be displayed next to its slider
+ * in the interface. It'd be nice if this could be a single function, but then
+ * Interface Builder bindings would no longer work.
+ */
 @property (retain,readonly) NSString *cameraActiveCoordinate2Label;
+
+/**\brief UI label text for the 3rd coordinate
+ *
+ * The label for the 3rd coordinate that should be displayed next to its slider
+ * in the interface. It'd be nice if this could be a single function, but then
+ * Interface Builder bindings would no longer work.
+ */
 @property (retain,readonly) NSString *cameraActiveCoordinate3Label;
+
+/**\brief UI label text for the 4th coordinate
+ *
+ * The label for the 4th coordinate that should be displayed next to its slider
+ * in the interface. It'd be nice if this could be a single function, but then
+ * Interface Builder bindings would no longer work.
+ */
 @property (retain,readonly) NSString *cameraActiveCoordinate4Label;
+
+/**\brief UI label text for the 5th coordinate
+ *
+ * The label for the 5th coordinate that should be displayed next to its slider
+ * in the interface. It'd be nice if this could be a single function, but then
+ * Interface Builder bindings would no longer work.
+ */
 @property (retain,readonly) NSString *cameraActiveCoordinate5Label;
+
+/**\brief UI label text for the 6th coordinate
+ *
+ * The label for the 6th coordinate that should be displayed next to its slider
+ * in the interface. It'd be nice if this could be a single function, but then
+ * Interface Builder bindings would no longer work.
+ */
 @property (retain,readonly) NSString *cameraActiveCoordinate6Label;
+
+/** \} */
+
+/**\defgroup osx-data-bindings-model Model selection
+ * \brief Model name and dimensions
+ *
+ * Used to set the intended model parameters and query the currently active
+ * ones; not all of these properties represent the current state, some retain
+ * the intended target state instead.
+ *
+ * \{
+ */
 
 @property (readwrite) NSInteger modelDepth;
 @property (readwrite) NSInteger renderDepth;
+@property (retain,readwrite) NSString *model;
+@property (retain,readwrite) NSString *selectedModelName;
+
+/** \} */
+
+/**\defgroup osx-data-bindings-parameters Model parameters
+ * \brief Model parameters
+ *
+ * \{
+ */
 
 @property (readwrite) double polarRadius;
 @property (readwrite) double polarPrecision;
@@ -118,10 +324,24 @@
 @property (readwrite) BOOL IFSPreRotate;
 @property (readwrite) BOOL IFSPostRotate;
 
-@property (retain,readwrite) NSString *model;
-@property (retain,readwrite) NSString *selectedModelName;
+/** \} */
 
-@property (retain,readwrite) IBOutlet NSPopUpButton *models;
+/**\defgroup osx-data-bindings-colours Colours
+ * \brief Render colours
+ *
+ * These colours are used in the process of rendering the models with OpenGL or
+ * when generating an SVG export.
+ *
+ * \{
+ */
+
+@property (retain,readwrite) NSColor *colourBackground;
+@property (retain,readwrite) NSColor *colourWire;
+@property (retain,readwrite) NSColor *colourSurface;
+
+/** \} */
+
+/** \} */
 
 - (BOOL)openFile:(NSString*)fullPath;
 - (IBAction)openDocument:(id)sender;
--- src/xstep/desktop/main.m
+++ src/xstep/desktop/main.m
@@ -1,3 +1,18 @@
+/**\ingroup topologic-frontend
+ * \defgroup frontend-cocoa OSX/Cocoa frontend
+ * \brief Native Topologic frontend for OSX
+ *
+ * This is a native Topologic frontend using Cocoa for Mac OSX. Most of the code
+ * is in the Topologic include files, so this is really just a fairly thin
+ * Objective C wrapper around the C++ core.
+ *
+ * \see http://itunes.apple.com/us/app/topologic/id558300160 - this frontend on
+ *      the Mac App Store. If you like this project then it'd be totally rad if
+ *      you considered buying it.
+ *
+ * \{
+ */
+
 /**\file
  * \brief Topologic/OSX main function
  *
@@ -34,3 +49,5 @@ int main(int argc, char *argv[])
 {
     return NSApplicationMain(argc, (const char **)argv);
 }
+
+/** \} */
