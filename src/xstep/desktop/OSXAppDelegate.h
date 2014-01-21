bc892b14f4296630e5bfe5ac54b8760c4d570225
Magnus Achim Deininger <magnus@ef.gy>
2014-01-21 13:15:06 +0000

    additional documentation

--- src/xstep/desktop/OSXAppDelegate.h
+++ src/xstep/desktop/OSXAppDelegate.h
@@ -50,11 +50,17 @@
  */
 @interface OSXAppDelegate : NSObject <NSApplicationDelegate>
 {
+    /**\brief OpenGL renderer instance
+     *
+     * Local variable that holds a pointer to the OpenGL renderer; accessed
+     * though the openGL property.
+     */
     OpenGLRenderer IBOutlet *openGL;
 
     NSInteger drawerMode;
     NSInteger activeCamera;
 
+/* the next three are probably not necessary and should be removed */
     NSColor *colourBackground;
     NSColor *colourWire;
     NSColor *colourSurface;
@@ -66,14 +72,26 @@
     NSPopUpButton IBOutlet *models;
 }
 
-@property (readonly) topologic::state<GLfloat,MAXDEPTH> *state;
-
+/**\brief Application window
+ *
+ * Bound to the application's window so that it can be modified at runtime.
+ */
 @property (assign) IBOutlet NSWindow *window;
+
+/**\brief List of models
+ *
+ * This is the complete list of available modules; The list is populated on the
+ * fly and this instance is bound with Interface Builder.
+ */
 @property (retain,readwrite) IBOutlet NSPopUpButton *models;
 
+/**\brief The OpenGL context
+ *
+ * This bound to the OpenGL render wrapper on programme startup, so that the
+ * delegate can tell the OpenGL context that it needs to draw itself anew.
+ */
 @property (retain,readwrite) OpenGLRenderer *openGL;
 
-@property (readwrite) BOOL fractalFlameColouring;
 @property (readonly) BOOL drawerVisible;
 @property (readonly) BOOL modelViewDrawerVisible;
 
@@ -103,6 +121,12 @@
  *
  * \{
  */
+
+/**\brief Topologic state object
+ *
+ * Allows access to the generic C++ state object instance for the application.
+ */
+@property (readonly) topologic::state<GLfloat,MAXDEPTH> *state;
 
 /**\brief Active camera
  *
@@ -302,9 +326,34 @@
  * \{
  */
 
+/**\brief (Intended) model depth
+ *
+ * Contains the (intended) model depth. The model depth is the '4' in '4-cube'.
+ */
 @property (readwrite) NSInteger modelDepth;
+
+/**\brief (Intended) render depth
+ *
+ * Contains the depth at which content should be rendered, i.e. how many
+ * coordinates a single (real) vector ought to have. This is different than the
+ * model depth, because it might be desirable to view models at a higher depth
+ * than would be strictly necessary - for example, one might want to view a
+ * 2-sphere in 4D to observe how it can be turned inside-out with rotations.
+ */
 @property (readwrite) NSInteger renderDepth;
+
+/**\brief (Intended) model name
+ *
+ * This is the 'name' of the model, e.g. 'cube' for a square, a cube, a
+ * tesseract, etc.
+ */
 @property (retain,readwrite) NSString *model;
+
+/**\brief Current qualified model name
+ *
+ * This is the qualified name of the current model, e.g. '4-cube' for a
+ * tesseract. This is only updated when a new model is actually set.
+ */
 @property (retain,readwrite) NSString *selectedModelName;
 
 /** \} */
@@ -312,17 +361,85 @@
 /**\defgroup osx-data-bindings-parameters Model parameters
  * \brief Model parameters
  *
+ * Groups the various model parameters together that change the appearance of a
+ * model, but do not contribute to selecting an actual model class to create the
+ * model's mesh.
+ *
  * \{
  */
 
+/**\brief Model radius
+ *
+ * The radius parameter of things like spheres. Also used with klein bagels and
+ * moebius strips. Recently this parameter also determines the size of cubes and
+ * planes, and by extension also the size of IFS elements, which use cubes or
+ * planes as their base object to deform.
+ */
 @property (readwrite) double polarRadius;
+
+/**\brief Precision
+ *
+ * This used to only control the precision of smooth surfaces by specifying the
+ * number of subdivisions for spheres, moebius strips and klein bagels, but now
+ * also controls the number of subdivisons of planes (and thus indirectly the
+ * fidelity of random flame geometry, as these make use of the planes).
+ */
 @property (readwrite) double polarPrecision;
+
+/**\brief Number of IFS iterations
+ *
+ * The number of iterations that an IFS is run for. The actual number of
+ * iterations may be lower than this, if running another iteration would exceed
+ * the OpenGL vertex limit.
+ */
 @property (readwrite) NSInteger IFSIterations;
+
+/**\brief Number of distinct IFS functions
+ *
+ * This is the number of functions to generate when creating random IFS
+ * geometry.
+ */
 @property (readwrite) NSInteger IFSFunctions;
+
+/**\brief IFS PRNG seed
+ *
+ * The seed value to use when generating random IFS geometry. This parameter
+ * makes it possible for random IFS geometry to be found again, if the seed is
+ * known.
+ */
 @property (readwrite) NSInteger IFSSeed;
+
+/**\brief Number of distinct flame variants to use
+ *
+ * Controls the number of distinct flame variants that have a weight other than
+ * zero when generating random fractal flame IFSs.
+ */
 @property (readwrite) NSInteger FlameVariants;
+
+/**\brief Allow pre-translation rotations
+ *
+ * When generating random IFS-based models, this flag controls whether a random
+ * rotation is applied to the IFS functions before their translation is applied.
+ */
 @property (readwrite) BOOL IFSPreRotate;
+
+/**\brief Allow post-translation rotations
+ *
+ * When generating random IFS-based models, this flag controls whether a random
+ * rotation is applied to the IFS functions after their translation is applied.
+ */
 @property (readwrite) BOOL IFSPostRotate;
+
+/**\brief Use fractal flame colouring algorithm
+ *
+ * When set to 'YES', the OpenGL renderer will apply an approximation of the
+ * fractal flame colouring algorithm to the output. This only produces
+ * reasonable output with IFS-based models, as it requires index metadata that
+ * is not present with other models.
+ *
+ * The SVG renderer ignores this flag.
+ */
+@property (readwrite) BOOL fractalFlameColouring;
 
 /** \} */
 
@@ -335,25 +452,146 @@
  * \{
  */
 
+/**\brief Background colour
+ *
+ * This is the background colour for the OpenGL canvas. Accesses the state
+ * object directly on queries.
+ */
 @property (retain,readwrite) NSColor *colourBackground;
+
+/**\brief Wireframe colour
+ *
+ * This is the wireframe colour for the OpenGL canvas. Accesses the state object
+ * directly on queries.
+ */
 @property (retain,readwrite) NSColor *colourWire;
+
+/**\brief Surface colour
+ *
+ * This is the surface colour for the OpenGL canvas. Accesses the state object
+ * directly on queries.
+ *
+ * \note This really should be something translucent, because higher-dimensional
+ *       geometry would be sort-of see-through from a higher-dimensional
+ *       perspective.
+ */
 @property (retain,readwrite) NSColor *colourSurface;
 
 /** \} */
 
 /** \} */
 
+/**\brief Open file
+ *
+ * Opens a file on the file system. The file should be an SVG with Topologic
+ * metadata, in which case this metadata is read from the file and applied to
+ * the active programme instance.
+ *
+ * \param[in] fullPath The file to open.
+ *
+ * \bug This function has quite possibly not been implemented.
+ */
 - (BOOL)openFile:(NSString*)fullPath;
+
+/**\brief Open URL
+ *
+ * Called when the file referenced by the given URL should be opened. The file
+ * should be an SVG with Topologic metadata, in which case this metadata is read
+ * from the file and applied to the active programme instance.
+ *
+ * \param[in] url The URL to open.
+ */
+- (void)openURL:(NSURL*)url;
+
+/**\brief Open file dialog
+ *
+ * Shows a dialog to open a file, then if one is selected it is read in and
+ * parsed as an SVG file with Topologic metadata.
+ *
+ * \param[in] sender Part of the Interface Builder action interface; ignored.
+ */
 - (IBAction)openDocument:(id)sender;
+
+/**\brief Save file dialog
+ *
+ * Shows a dialog to choose a file to save to. If a target is chosen, the
+ * programme creates an SVG with Topologic metadata that can be read back in.
+ * Saving to SVGs does not support all of the features supported by the OpenGL
+ * renderer.
+ *
+ * \param[in] sender Part of the Interface Builder action interface; ignored.
+ */
 - (IBAction)saveDocumentAs:(id)sender;
+
+/**\brief Reset coordinates to 1
+ *
+ * Reset all coordinates of the currently active camera to '1'.
+ *
+ * \param[in] sender Part of the Interface Builder action interface; ignored.
+ */
 - (IBAction)reset1:(id)sender;
+
+/**\brief Reset coordinates to PI/2
+ *
+ * Reset all coordinates of the currently active camera to 'PI/2'.
+ *
+ * \param[in] sender Part of the Interface Builder action interface; ignored.
+ */
 - (IBAction)resetPIHalf:(id)sender;
+
+/**\brief Generate random flame colours
+ *
+ * Reset the colour map for the fractal flame colouring algorithm.
+ *
+ * \param[in] sender Part of the Interface Builder action interface; ignored.
+ */
 - (IBAction)randomFlameColours:(id)sender;
+
+/**\brief Cocoa callback: do close application
+ *
+ * Tells Cocoa that we really do want to exit the applciation if the last window
+ * is closed. The default is to keep the application running.
+ */
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication;
+
+/**\brief Go to website
+ *
+ * Opens a browser window and jumps to Topologic's web site, which may contain
+ * information about updates or links to other goodies.
+ *
+ * \param[in] sender Part of the Interface Builder action interface; ignored.
+ */
 - (IBAction)goToWebsite:(id)sender;
+
+/**\brief Update OpenGL view
+ *
+ * Used to tell the OpenGL view that it should draw itself anew.
+ *
+ * \param[in] sender Part of the Interface Builder action interface; ignored.
+ */
 - (IBAction)updateOpenGLView:(id)sender;
+
+/**\brief Save file
+ *
+ * Renders an SVG with Topologic metadata embedded to the specified file.
+ *
+ * \param[in] fileName Where to put the new SVG to.
+ */
 - (void) saveFile:(NSString*)fileName;
+
+/**\brief Update model
+ *
+ * Called after model parameters are changed. This will tell Topologic to use
+ * its model factory to recreate a new model with the currently intended
+ * parameters.
+ */
 - (void) updateModel;
+
+/**\brief Recalculate model
+ *
+ * Like 'updateModel', but the model is merely told to recalculate its mesh.
+ * This should be called after changing model parameters.
+ */
 - (void) updateModelParameters;
 
 @end
